cbdb054fa5d1dfecc1abb0d9372ff082b6bfd409
Magda <magdagozdalik28@gmail.com>
2022-02-23 12:53:30 +0000

    Sass file structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-    <h2>Hello gmail clone</h2>
+    <Header />
     </div>
   );
 }
