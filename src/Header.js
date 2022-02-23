18c5d457e40d62f9d0fee9eceb1b0bbceb6405d0
Magda <magdagozdalik28@gmail.com>
2022-02-23 21:30:53 +0000

    Header updates

--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import AppsIcon from '@material-ui/icons/Apps';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import { IconButton, Avatar } from '@material-ui/core';
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import { IconButton, Avatar } from "@material-ui/core";
 import gmail_logo from "./images/gmail_logo.png";
 
 function Header() {
   return (
-   <header className="header">
-    <div className="header__left">
-     <IconButton>
-     <MenuIcon />
-    </IconButton>
-    <img src={gmail_logo} alt="gmail logo" />
-    </div>
-    <div className="header__middle">
-     <SearchIcon />
-     <input placeholder="Search mail" type="text" />
-     <ArrowDropDownIcon />
-    </div>
-    <div className="header__right">
-     <AppsIcon />
-     <NotificationsIcon />
-     <Avatar />
-
-    </div>
-    
-    
-   </header>
-  )
+    <header className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img src={gmail_logo} alt="gmail logo" />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon />
+      </div>
+      <div className="header__right">
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
